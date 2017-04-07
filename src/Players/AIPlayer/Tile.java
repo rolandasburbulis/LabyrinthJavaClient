9055ce3427fa31b875d44a1427fc467a6bbfae8e
Rolandas Burbulis <rolandasburbulis@gmail.com>
2017-04-06 23:26:16 -0400

    RBurbulis - Tile.hasExit(CompassDirection) now uses a static look up table instead of a bunch of switch statements for efficiency.

--- src/Players/AIPlayer/Tile.java
+++ src/Players/AIPlayer/Tile.java
@@ -14,6 +14,40 @@ public class Tile implements Serializable {
     private MazePathOrientation mazePathOrientation;
     private TreasureType treasureType;
     private Set<Integer> players;
+    private static Set<String> hasExitLookupTable;
+
+    static {
+        hasExitLookupTable = new HashSet<>();
+
+        hasExitLookupTable.add(MazePathType.I.toString() + MazePathOrientation.ZERO.toString() + CompassDirection.NORTH.toString());
+        hasExitLookupTable.add(MazePathType.I.toString() + MazePathOrientation.ZERO.toString() + CompassDirection.SOUTH.toString());
+        hasExitLookupTable.add(MazePathType.I.toString() + MazePathOrientation.ONE_HUNDRED_EIGHTY.toString() + CompassDirection.NORTH.toString());
+        hasExitLookupTable.add(MazePathType.I.toString() + MazePathOrientation.ONE_HUNDRED_EIGHTY.toString() + CompassDirection.SOUTH.toString());
+        hasExitLookupTable.add(MazePathType.I.toString() + MazePathOrientation.NINETY.toString() + CompassDirection.EAST.toString());
+        hasExitLookupTable.add(MazePathType.I.toString() + MazePathOrientation.NINETY.toString() + CompassDirection.WEST.toString());
+        hasExitLookupTable.add(MazePathType.I.toString() + MazePathOrientation.TWO_HUNDRED_SEVENTY.toString() + CompassDirection.EAST.toString());
+        hasExitLookupTable.add(MazePathType.I.toString() + MazePathOrientation.TWO_HUNDRED_SEVENTY.toString() + CompassDirection.WEST.toString());
+        hasExitLookupTable.add(MazePathType.L.toString() + MazePathOrientation.ZERO.toString() + CompassDirection.WEST.toString());
+        hasExitLookupTable.add(MazePathType.L.toString() + MazePathOrientation.ZERO.toString() + CompassDirection.NORTH.toString());
+        hasExitLookupTable.add(MazePathType.L.toString() + MazePathOrientation.NINETY.toString() + CompassDirection.NORTH.toString());
+        hasExitLookupTable.add(MazePathType.L.toString() + MazePathOrientation.NINETY.toString() + CompassDirection.EAST.toString());
+        hasExitLookupTable.add(MazePathType.L.toString() + MazePathOrientation.ONE_HUNDRED_EIGHTY.toString() + CompassDirection.EAST.toString());
+        hasExitLookupTable.add(MazePathType.L.toString() + MazePathOrientation.ONE_HUNDRED_EIGHTY.toString() + CompassDirection.SOUTH.toString());
+        hasExitLookupTable.add(MazePathType.L.toString() + MazePathOrientation.TWO_HUNDRED_SEVENTY.toString() + CompassDirection.SOUTH.toString());
+        hasExitLookupTable.add(MazePathType.L.toString() + MazePathOrientation.TWO_HUNDRED_SEVENTY.toString() + CompassDirection.WEST.toString());
+        hasExitLookupTable.add(MazePathType.T.toString() + MazePathOrientation.ZERO.toString() + CompassDirection.NORTH.toString());
+        hasExitLookupTable.add(MazePathType.T.toString() + MazePathOrientation.ZERO.toString() + CompassDirection.SOUTH.toString());
+        hasExitLookupTable.add(MazePathType.T.toString() + MazePathOrientation.ZERO.toString() + CompassDirection.WEST.toString());
+        hasExitLookupTable.add(MazePathType.T.toString() + MazePathOrientation.NINETY.toString() + CompassDirection.NORTH.toString());
+        hasExitLookupTable.add(MazePathType.T.toString() + MazePathOrientation.NINETY.toString() + CompassDirection.EAST.toString());
+        hasExitLookupTable.add(MazePathType.T.toString() + MazePathOrientation.NINETY.toString() + CompassDirection.WEST.toString());
+        hasExitLookupTable.add(MazePathType.T.toString() + MazePathOrientation.ONE_HUNDRED_EIGHTY.toString() + CompassDirection.NORTH.toString());
+        hasExitLookupTable.add(MazePathType.T.toString() + MazePathOrientation.ONE_HUNDRED_EIGHTY.toString() + CompassDirection.EAST.toString());
+        hasExitLookupTable.add(MazePathType.T.toString() + MazePathOrientation.ONE_HUNDRED_EIGHTY.toString() + CompassDirection.SOUTH.toString());
+        hasExitLookupTable.add(MazePathType.T.toString() + MazePathOrientation.TWO_HUNDRED_SEVENTY.toString() + CompassDirection.EAST.toString());
+        hasExitLookupTable.add(MazePathType.T.toString() + MazePathOrientation.TWO_HUNDRED_SEVENTY.toString() + CompassDirection.SOUTH.toString());
+        hasExitLookupTable.add(MazePathType.T.toString() + MazePathOrientation.TWO_HUNDRED_SEVENTY.toString() + CompassDirection.WEST.toString());
+    }
 
     public Tile(final MazePathType mazePathType,
                 final TreasureType treasureType) {
@@ -78,126 +112,12 @@ public class Tile implements Serializable {
     }
 
     public boolean hasExit(final CompassDirection compassDirection) {
-        boolean hasExit = false;
-
         if(this.mazePathOrientation == null) {
             throw new IllegalStateException("This tile does not have an orientation set, therefore checking if it has an exit in a particular compass direction is not valid.");
         }
 
-        switch (this.mazePathType) {
-            case I:
-                switch (this.mazePathOrientation) {
-                    case ZERO:
-                    case ONE_HUNDRED_EIGHTY:
-                        switch (compassDirection) {
-                            case NORTH:
-                            case SOUTH:
-                                hasExit = true;
-                                break;
-                        }
-
-                        break;
-                    case NINETY:
-                    case TWO_HUNDRED_SEVENTY:
-                        switch (compassDirection) {
-                            case EAST:
-                            case WEST:
-                                hasExit = true;
-                                break;
-                        }
-
-                        break;
-                }
-
-                break;
-            case L:
-                switch (this.mazePathOrientation) {
-                    case ZERO:
-                        switch (compassDirection) {
-                            case WEST:
-                            case NORTH:
-                                hasExit = true;
-                                break;
-                        }
-
-                        break;
-                    case NINETY:
-                        switch (compassDirection) {
-                            case NORTH:
-                            case EAST:
-                                hasExit = true;
-                                break;
-                        }
-
-                        break;
-                    case ONE_HUNDRED_EIGHTY:
-                        switch (compassDirection) {
-                            case EAST:
-                            case SOUTH:
-                                hasExit = true;
-                                break;
-                        }
-
-                        break;
-                    case TWO_HUNDRED_SEVENTY:
-                        switch (compassDirection) {
-                            case SOUTH:
-                            case WEST:
-                                hasExit = true;
-                                break;
-                        }
-
-                        break;
-                }
-
-                break;
-            case T:
-                switch(this.mazePathOrientation) {
-                    case ZERO:
-                        switch (compassDirection) {
-                            case NORTH:
-                            case SOUTH:
-                            case WEST:
-                                hasExit = true;
-                                break;
-                        }
-
-                        break;
-                    case NINETY:
-                        switch (compassDirection) {
-                            case NORTH:
-                            case EAST:
-                            case WEST:
-                                hasExit = true;
-                                break;
-                        }
-
-                        break;
-                    case ONE_HUNDRED_EIGHTY:
-                        switch (compassDirection) {
-                            case NORTH:
-                            case EAST:
-                            case SOUTH:
-                                hasExit = true;
-                                break;
-                        }
-
-                        break;
-                    case TWO_HUNDRED_SEVENTY:
-                        switch (compassDirection) {
-                            case EAST:
-                            case SOUTH:
-                            case WEST:
-                                hasExit = true;
-                                break;
-                        }
-
-                        break;
-                }
-
-                break;
-        }
-
-        return hasExit;
+        return Tile.hasExitLookupTable.contains(this.mazePathType.toString() +
+                                                this.mazePathOrientation.toString() +
+                                                compassDirection.toString());
     }
 }
